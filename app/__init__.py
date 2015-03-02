48a395e7e30744d10ddf3b0683d6459fbf289fa2
wellesleywatson <git@github.com:wellesleywatson>
2015-03-02 01:41:49 +0000

    added heroku db info

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ UPLOAD_FOLDER = 'app/static/img/profile_pics'
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rsrywhxhvptyfs:4YHUoPgsPJQb4hsVEaUwsjEywd@ec2-54-197-249-212.compute-1.amazonaws.com:5432/d7m2kn564j8j0s'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://action@localhost/action'
 app.config['SECRET_KEY'] = 'super secret key'
 app.config.from_object('confg')
 db = SQLAlchemy(app)
